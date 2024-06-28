20e3bbf7163df8c92e3e8cc377b38a40aca18fda
boi-git <adhwa.aizat23@gmail.com>
2024-06-28 09:37:01 +0800

    Update Router

--- food-order/src/rounter.jsx
+++ food-order/src/rounter.jsx
@@ -1,5 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from "./views/login";
+import User from "./views/user";
+import Dashboard from "./views/dashboard";
+import Restaurant from "./views/restaurant/restaurant";
+import NotFound from "./views/notfound";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/user",
+        element: <User />,
+    },
+    {
+        path: "/dashboard",
+        element: <Dashboard />,
+    },
+    {
+        path: "/restaurant",
+        element: <Restaurant />,
+    },
+    {
+        path: "*",
+        element: <NotFound />,
+    },
+]);
 
 export default router;
